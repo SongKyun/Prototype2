1030bf0950fe51598c71f70cd1a7dc1373abc1a1
Song-Kyun <89376275+SongKyun@users.noreply.github.com>
2023-06-07 08:27:35 +0900

    23.6.7
    2단원 과제2 물어 오기 놀이

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject DogPrefab;
+
+    public KeyCode triggerKey = KeyCode.Space; // 동작 트리거 키
+    public float delayTime = 0.5f; // 딜레이 시간
+
+    private bool CanDogShot = true; // 강아지슛 수행 여부
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        // 키 동작 후에, 수행할 수 있는 상태인지 확인
+        if (Input.GetKeyDown(triggerKey) && CanDogShot)
+        {
+            Instantiate(DogPrefab, transform.position, DogPrefab.transform.rotation);
+
+            CanDogShot = false;
+
+            StartCoroutine(EnableActionAfterDelay());
+        }
+    }
+
+    private IEnumerator EnableActionAfterDelay()
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        CanDogShot = true;
+    }
+ }
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] BallPrefab;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 첫 번째 생성을 위한 랜덤 시간 함수 호출
+        Invoke("SpawnRandomBall", Random.Range(startDelay, spawnInterval));
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        int BallIndex = Random.Range(0, BallPrefab.Length);
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(BallPrefab[BallIndex], spawnPos, BallPrefab[BallIndex].transform.rotation);
+
+        // 다음 생성을 위한 랜덤 시간 함수 호출
+        Invoke("SpawnRandomBall", Random.Range(startDelay, spawnInterval));
+    }
+}
