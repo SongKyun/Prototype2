198efcb7ecab52d46b72dc4baf93f88aaab2ccb7
Song-Kyun <89376275+SongKyun@users.noreply.github.com>
2023-07-17 22:41:02 +0900

    23 7 17
    23 7 17

--- Assets/Scripts/DestroyOutofBounds.cs
+++ Assets/Scripts/DestroyOutofBounds.cs
@@ -1,9 +1,8 @@
-using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyOutofBounds : MonoBehaviour
+public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
     private float lowerBound = -10;
@@ -11,21 +10,26 @@ public class DestroyOutofBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If an object goes past the players view in the game, remove that object
         if (transform.position.z > topBound)
         {
-            Destroy(gameObject);
+            // Instead of destroying the projectile when it leaves the screen
+            //Destroy(gameObject);
+
+            // Just deactivate it
+            gameObject.SetActive(false);
+
         }
         else if (transform.position.z < lowerBound)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
+
     }
 }
